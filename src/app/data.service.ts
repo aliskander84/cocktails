3bfeee8cd751b360033cb28d606327cb672e1b50
alexchegash <aliskander84@gmail.com>
2020-06-03 11:37:32 +0300

    wrote readme

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,12 @@
 import {EventEmitter, Injectable, Output} from '@angular/core'
 import {ApiService} from './api.service'
-import {TInputCategories, TCategories, TCategory, TInputCocktails, TCocktails} from './types'
+import {
+  TInputCategories,
+  TCategories,
+  TCategory,
+  TInputCocktails,
+  TCocktails
+} from './types'
 
 @Injectable({
   providedIn: 'root'
@@ -21,13 +27,11 @@ export class DataService {
 
   getData() {
     this.api.getCategories().subscribe(({drinks}: TInputCategories) => {
-      // console.log('getCategories')
       this.allCategories = this.selectedCategories = drinks.map(({strCategory}) => {
         return {category: strCategory, selected: true}
       })
       this.allCategories$.emit(this.allCategories)
       this.allCategories.forEach(({category}: TCategory) => {
-        // console.log('forEach')
         this.api.getCocktails(category).subscribe(({drinks}: TInputCocktails) => {
           this.allCocktails.push({
             category,
@@ -38,19 +42,29 @@ export class DataService {
           this.selectedCocktails$.emit(this.allCocktails)
         })
       })
-      // console.log(this.allCocktails)
-      // this.selectedCategories$.emit(this.selectedCategories)
-      // console.log(this.selectedCategories)
     })
 
   }
 
   selectCategories(categories: TCategories) {
     this.selectedCategories = this.allCategories.filter(({selected}: TCategory) => {
+      console.log(selected)
       return selected
     })
-    this.selectedCategories$.emit(this.selectedCategories)
+    this.selectCocktails(this.selectedCategories)
+    this.selectedCocktails$.emit(this.selectedCocktails)
   }
 
+  selectCocktails(categories: TCategories) {
+    this.selectedCocktails = this.allCocktails.filter(({category}) => {
+      let isMatched = false
+      categories.forEach(({category: c}: TCategory) => {
+        if (c === category) {
+          isMatched = true
+        }
+      })
+      return isMatched
+    })
+  }
 
 }
